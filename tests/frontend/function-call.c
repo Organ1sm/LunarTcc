54654d68f876ded3f5f0eedce1134eb17f64b478
Organ1sm <ywwithpeople@gmail.com>
2022-10-20 18:13:36 +0800

    [Test]: Adding automatic test python scripts.

--- tests/frontend/function-call.c
+++ tests/frontend/function-call.c
@@ -1,3 +1,3 @@
-int bar(int a, double b) { return 1; }
+int bar(int a) { return 1; }
 
-int foo() { return bar(1 + 2, 2.0); }
+int foo() { return bar(1 + 2); }
--- tests/frontend/function-with-local-var.c
+++ tests/frontend/function-with-local-var.c
@@ -1,5 +1,10 @@
-int foo()
+// RUN: AArch64
+// FUNC-DECL: int test()
+// TEST-CASE: test() -> 5
+
+int test()
 {
     int a;
+    a = 5;
     return a;
 }
