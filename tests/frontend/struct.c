29ce8a48f1af2be5ec417cf27682dd61351377cd
Organ1sm <ywwithpeople@gmail.com>
2022-10-21 17:15:01 +0800

    [FrontEnd]: Improved `struct` type handing.

--- tests/frontend/struct.c
+++ tests/frontend/struct.c
@@ -9,14 +9,21 @@ struct Point
     int z;
 };
 
-// TODO: test this also when function calling implemented
-int testS(struct Point P) { return P.x + P.z; }
+int StructAdd(struct Point P) { return P.x + P.z; }
+
+struct Point CreateStruct()
+{
+    struct Point P;
+
+    P.x = 1;
+    P.z = 2;
+
+    return P;
+}
 
 int test()
 {
     struct Point P;
-    P.x = 1;
-    P.z = 2;
-
-    return testS(P);
+    P = CreateStruct();
+    return StructAdd(P);
 }
