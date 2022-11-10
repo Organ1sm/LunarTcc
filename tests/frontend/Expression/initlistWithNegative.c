1223b4fb29c8002573f2e430717a7a5b7713615c
Organ1sm <ywwithpeople@gmail.com>
2022-11-10 17:21:07 +0800

    [SyntaxSupport/MiddleEnd]: Adding support for negative constant generation, initialize 1D array with negative constant list.

---
+++ tests/frontend/Expression/initlistWithNegative.c
@@ -0,0 +1,13 @@
+// RUN: AArch64
+// FUNC-DECL: int test()
+// TEST-CASE: test() -> 4
+
+
+int test()
+{
+    int found_lines_indices[4] = {-1, -2, -3, -4};
+
+    return found_lines_indices[3];
+}
+
+void test1() { int a = 1; }
