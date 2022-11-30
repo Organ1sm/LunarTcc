91306c506f908520fb7c165c8f4011641bb4876d
Organ1sm <ywwithpeople@gmail.com>
2022-11-30 18:23:42 +0800

    [BackEnd/AArch64]: Adding support for S and D registers.

--- include/BackEnd/TargetArchs/AArch64/AArch64RegisterInfo.hpp
+++ include/BackEnd/TargetArchs/AArch64/AArch64RegisterInfo.hpp
@@ -8,8 +8,11 @@ namespace AArch64
 {
     enum Registers : unsigned {
         Invalid,
+
 #define AARCH64_REGISTER(ID, WIDTH, NAME, ALIAS) ID,
 #include "AArch64Registers.def"
+
+        RegisterEnd,
     };
 
     class AArch64RegisterInfo : public RegisterInfo
@@ -28,7 +31,13 @@ namespace AArch64
         unsigned GetZeroRegister(const unsigned BitWidth) override;
         unsigned GetStructPtrRegister() override;
 
+        unsigned GetRegisterClass(const unsigned BitWidth, const bool IsFP) override;
+        std::string GetRegClassString(const unsigned RegClass) override;
+        unsigned GetRegClassFromReg(const unsigned Reg) override;
+        unsigned GetRegClassRegsSize(const unsigned RegClass) override;
+
       private:
-        TargetRegister Registers[67];
+        TargetRegister Registers[RegisterEnd - 1];
+        std::vector<std::string> RegClassEnumStrings;
     };
 }    // namespace AArch64
