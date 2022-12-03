ca314f369f85219eba4cdd5a166f367b8e956b61
Organ1sm <ywwithpeople@gmail.com>
2022-12-03 12:18:54 +0800

    [MiddleEnd]: Fix the struct alignment issue.

--- lib/MiddleEnd/IR/IRType.cpp
+++ lib/MiddleEnd/IR/IRType.cpp
@@ -2,6 +2,7 @@
 // Created by Organ1sm.
 //
 #include "MiddleEnd/IR/IRType.hpp"
+#include "BackEnd/Support.hpp"
 #include "BackEnd/TargetMachine.hpp"
 #include "fmt/core.h"
 #include "fmt/format.h"
@@ -25,6 +26,46 @@ void IRType::ReduceDimension()
         Dimensions.erase(Dimensions.begin());
 }
 
+unsigned IRType::GetStructMaxAlignment(TargetMachine *TM) const
+{
+    unsigned Alignment = 1;
+
+    for (auto &type : MembersTypeList)
+    {
+        if (type.IsPointer())
+        {
+            unsigned PtrSize = TM ? TM->GetPointerSize() / 8 : 8;
+            Alignment        = std::max(Alignment, PtrSize);
+        }
+        else if (type.IsArray())
+        {
+            if (type.GetBaseType().IsScalar())
+            {
+                Alignment =
+                    std::max(Alignment,
+                             static_cast<unsigned>(type.GetBaseType().GetByteSize(TM)));
+            }
+            else
+                assert(!"Unhandled array base type");
+        }
+        else if (type.IsScalar())
+        {
+            Alignment = std::max(Alignment, static_cast<unsigned>(type.GetByteSize(TM)));
+        }
+        else if (type.IsStruct())
+        {
+            Alignment = std::max(Alignment,
+                                 static_cast<unsigned>(type.GetStructMaxAlignment(TM)));
+        }
+        else
+        {
+            assert(!"Unhandled type.");
+        }
+    }
+
+    return Alignment;
+}
+
 std::size_t IRType::GetByteSize(TargetMachine *TM) const
 {
     unsigned NumberOfElements = 1;
@@ -33,12 +74,19 @@ std::size_t IRType::GetByteSize(TargetMachine *TM) const
         for (std::size_t i = 0; i < Dimensions.size(); i++)
             NumberOfElements *= Dimensions[i];
 
-    if (IsStruct())
+    if (IsStruct() && IsPointer())
     {
-        unsigned Result = 0;
-        for (auto &t : MembersTypeList)
-            Result += t.GetByteSize();
+        unsigned Result          = 0;
+        const unsigned Alignment = GetStructMaxAlignment(TM);
 
+        for (auto &t : MembersTypeList)
+        {
+            auto Size = t.GetByteSize();
+            Result    = GetNextAlignedValue(Result, Size);
+            Result += Size;
+        }
+
+        Result = GetNextAlignedValue(Result, Alignment);
         return Result;
     }
 
@@ -60,19 +108,29 @@ unsigned IRType::CalcElemSize(unsigned int dim)
 
     assert(dim == 0 || dim < Dimensions.size() && "Out of bound");
 
-    for (std::size_t i = dim + 1; i < Dimensions.size(); i++)
+    std::size_t i = PointerLevel > 1 ? dim : dim + 1;
+    for (; i < Dimensions.size(); i++)
         result *= Dimensions[i];
 
     return result * (BitWidth / 8);
 }
 
-unsigned IRType::GetElemByteOffset(const unsigned int StructElemIndex) const
+unsigned IRType::GetElemByteOffset(const unsigned int StructElemIndex,
+                                   TargetMachine *TM) const
 {
     assert(StructElemIndex < MembersTypeList.size() && "Out of bound");
 
     unsigned ByteOffset = 0;
     for (std::size_t i = 0; i < StructElemIndex; ++i)
-        ByteOffset += MembersTypeList[i].GetByteSize();
+    {
+        const unsigned Size = MembersTypeList[i].GetByteSize();
+        ByteOffset          = GetNextAlignedValue(ByteOffset, Size);
+
+        ByteOffset += Size;
+    }
+
+    const unsigned MaxAlignment = GetStructMaxAlignment(TM);
+    ByteOffset                  = GetNextAlignedValue(ByteOffset, MaxAlignment);
 
     return ByteOffset;
 }
@@ -103,4 +161,12 @@ std::string IRType::AsString() const
     std::string PtrStr(PointerLevel, '*');
 
     return Str + PtrStr;
+}
+
+std::size_t IRType::GetBaseTypeByteSize(TargetMachine *TM) const
+{
+    auto CopyIRType = *this;
+    CopyIRType.SetPointerLevel(0);
+
+    return CopyIRType.GetByteSize(TM);
 }
